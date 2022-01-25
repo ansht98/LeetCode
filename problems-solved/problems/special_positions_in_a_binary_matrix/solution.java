913b69bbc79bbddb2c8238f5b01b21d8a81a92e7
ansht98 <97855094+ansht98@users.noreply.github.com>
2022-01-25 19:54:54 +0000

    Sync LeetCode submission - Special Positions in a Binary Matrix (java)

--- problems-solved/problems/special_positions_in_a_binary_matrix/solution.java
+++ problems-solved/problems/special_positions_in_a_binary_matrix/solution.java
@@ -1,26 +1,24 @@
 class Solution {
     public int numSpecial(int[][] mat) {
-        int sum = 0;
-        int index = 0;
-        int count = 0;
-        for (int i =0; i < mat.length; i++) {
+       int[] a = new int[mat.length];
+        int[] b = new int[mat[0].length];
+        int count  = 0;
+        for (int i = 0; i < mat.length; i++) {
             for (int j = 0; j < mat[0].length; j++) {
-                sum = sum + mat[i][j];
                 if (mat[i][j] == 1) {
-                    index = j;
+                    a[i]++;
+                    b[j]++;
                 }
             }
-            if (sum == 1) {
-                sum = 0;
-                for (int k = 0; k < mat.length;k++) {
-                    sum = sum + mat[k][index];
-                    
-                }
-                if (sum == 1) {
-                    count++;
+        }
+        
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                if (mat[i][j] == 1) {
+                    if (a[i] == 1 && b[j] == 1)
+                        count++;
                 }
             }
-            sum = 0;
         }
         return count;
     }
