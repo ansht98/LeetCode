5fdb7775dfa262162628f089524545da7e82bcf4
ansht98 <97855094+ansht98@users.noreply.github.com>
2022-12-28 04:02:29 +0000

    Sync LeetCode submission - Sort Array By Parity (java)

--- problems-solved/problems/sort_array_by_parity/solution.java
+++ problems-solved/problems/sort_array_by_parity/solution.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
-        int n = nums.length;
+        /*int n = nums.length;
         int j = n-1;
         int temp = 0;
         int even = 0;
@@ -21,6 +21,30 @@ class Solution {
                 x--;
             }
         }
-        return ans3;
+        return ans3;*/
+        //Arrays.sort(nums);
+        int n = nums.length;
+        int i = 0;
+        int j = n-1;
+        while (i<j) {
+            if (nums[i]%2==0&&nums[j]%2==1) {
+                i++;
+                j--;
+            }
+            else if (nums[i]%2==1&&nums[j]%2==0) {
+                int temp = nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+                i++;
+                j--;
+            }
+            else if (nums[i]%2==1&&nums[j]%2==1) {
+                j--;
+            }
+            else if (nums[i]%2==0&&nums[j]%2==0) {
+                i++;
+            }
+        }
+        return nums;
     }
 }
