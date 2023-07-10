b82bca0558754c24e0f7265811260f40e1c2ae55
ansht98 <97855094+ansht98@users.noreply.github.com>
2023-07-10 01:44:53 +0000

    Sync LeetCode submission - Search Insert Position (java)

---
+++ problems-solved/problems/search_insert_position/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int l = 0;
+        int h = nums.length-1;
+        while (l <= h) {
+            
+            if (nums[(l+h)/2] == target) {
+                return (l+h)/2;
+            }
+            if (nums[(l+h)/2] > target) {
+                h = ((l+h)/2)-1;
+            }
+            if (nums[(l+h)/2] < target) {
+                l = ((l+h)/2)+1;
+            }
+
+        }
+        return l;
+    }
+}
